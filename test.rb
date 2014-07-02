d57be928294650fee13d9895f024a6b4f9bf1da3
bestdramana <bestdramana@github.com>
2014-07-01 22:52:54 -0400

    format

--- test.rb
+++ test.rb
@@ -5,10 +5,6 @@ patterns = HTML::Patterns.new
 
 hmd = CONVERTER::HtmlToMd.new("../../FileOut.html")
 
-preTagReg = /<(ul|ol|)\b[^>]*>([\s\S]*)<\/|ul|ol>/
-
-match = preTagReg.match("<ol><li>asd</li><li>asd</li></ol>")
-
 hmd.readHtmlFile()
 #hmd.showRawContent()
 hmd.convert()
